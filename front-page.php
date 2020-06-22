c91538c0c37d8e71681e664f4036f01e15ad8ef5
Ian Hernandez <ian.hernandez@dreamhost.com>
2020-06-22 16:34:00 -0700

    simplify frontpage loop

--- front-page.php
+++ front-page.php
@@ -110,22 +110,9 @@ data & support to build their brand and increase their revenue.</p>
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
 			endwhile;
-
-			the_posts_navigation();
-
 		else :
-
 			get_template_part( 'template-parts/content', 'none' );
-
 		endif;
 		?>
 
