8bbfa884d623d0d35c1b9173d7e313482560a25e
Ian Hernandez <ian.hernandez@dreamhost.com>
2020-06-22 16:13:30 -0700

    bring in front page content

--- front-page.php
+++ front-page.php
@@ -15,6 +15,90 @@
 get_header();
 ?>
 
+
+
+
+<main>
+  <section class="intro">
+    <h1>A Modern Digital Model Agency</h1>
+    <p>We empower our partners by providing the right tools, guidance, 
+data & support to build their brand and increase their revenue.</p>
+  </section>
+  
+  <section class="secondary">
+    <img src="https://source.unsplash.com/ear8VOL7x8k/1400x2200" class="" alt="">
+    <div class="stack block-2" style="--stack-space: 2rem;">
+      <h2>Empowering Creators Every Step of the Way</h2>
+      <p>OF AGENCY is a new agency in Los Angeles, California with a strength in marketing, management, discovering opportunities & deep industry knowledge.</p>
+      <div class="social">
+        <i class="fab fa-snapchat"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-facebook"></i>
+      </div>
+      <p>We will maximize your potential on every platform. Let our copywriters, designers, and content experts</p>
+    </div>
+    
+  <div class="amarillo stack block-3" style="--stack-space: 2rem;">
+    <h2>Data Driven, Market Focused, Design Oriented</h2>
+    <div class="flex align-center">
+      <i class="ring fas fa-analytics"></i>
+      <p>Lorem ipsum dolor sit amet, ut aeque iudicabit his, ad usu eros simul. Cu prima partem dolorem vix. Errem soleat bonorum nec in.</p>
+    </div>
+    <a class="button" href="#">How To Join</a>
+  </div>
+</section>
+  
+  <section class="super-guide">
+    <div class="super-guide__form stack" style="--stack-space: 2rem;">
+      <h3>Download Our OnlyFans<br>Super Guide Free!</h3>
+      <p>Sign up for free access to our Super Guide, tried and true practices to grow we’ve discovered. <a href="#">Learn More</a></p>
+      <form class="form-email">
+        <input type="text" class="input" value="Chris">
+        <input type="submit" class="button" value="Email Guide" />
+      </form>
+    </div>
+    
+    <img src="https://source.unsplash.com/ZvbhojfQ2g4/1200x1600/" class="" alt="">
+    
+  </section>
+  <section class="research">
+        
+    <img src="https://source.unsplash.com/-ldsHdX28OI/1200x1800/" class="" alt="">
+    
+    <div class="research-blurb stack" style="--stack-space: 2rem;">
+      <h2>Research, Data, & Science to Make More Money Online</h2>
+      <p>Sign up for free access to our Super Guide, tried and true practices to grow we’ve discovered. </p>
+      <div class="flex align-center">
+      <i class="ring fas fa-money-bill"></i>
+    <p>Lorem ipsum dolor sit amet, ut aeque iudicabit his, ad usu eros simul. Cu prima partem dolorem vix. Errem soleat bonorum nec in.</p>
+    </div>
+      <a class="button" href="#">Learn More</a>
+    </div>
+
+  </section>
+    
+    <section class="offer">
+        
+    
+    
+    <div class="stack offer-blurb" style="--stack-space: 2rem;">
+      <h2>Research, Data, & Science to Make More Money Online</h2>
+      <p>Sign up for free access to our Super Guide, tried and true practices to grow we’ve discovered. </p>
+      <div class="flex align-center">
+      <i class="ring fas fa-money-bill"></i>
+    <p>Lorem ipsum dolor sit amet, ut aeque iudicabit his, ad usu eros simul. Cu prima partem dolorem vix. Errem soleat bonorum nec in.</p>
+    </div>
+      <a class="button" href="#">Learn More</a>
+    </div>
+    <img src="https://source.unsplash.com/vxMjD4_92g0/1200x1600/" class="" alt="">
+  </section>
+  
+  
+</main>
+
+
+
 <div class="grid-layout--1 container">
 	<main id="primary" class="site-main main-front-page">
 	
@@ -60,10 +144,6 @@ get_header();
 		?>
 
 	</main><!-- #main -->
-
-<?php
-get_sidebar();
-?>
 
 </div>
 
