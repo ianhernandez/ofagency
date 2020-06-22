448744e7b31b7e0d09a2c92b5c5f201708a59d38
Ian Hernandez <ian.hernandez@dreamhost.com>
2020-06-22 14:29:39 -0700

    single page class

--- single.php
+++ single.php
@@ -10,6 +10,8 @@
 get_header();
 ?>
 
+<div class="main-wrapper">
+
 	<main id="primary" class="site-main">
 
 		<?php
@@ -35,6 +37,8 @@ get_header();
 
 	</main><!-- #main -->
 
+	<?php get_sidebar(); ?>
+</div>
+
 <?php
-get_sidebar();
 get_footer();
