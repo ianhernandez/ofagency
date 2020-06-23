1fde43a1fdbf89b2ef0dbee0e779caa2937d7431
Ian Hernandez <ian.hernandez@dreamhost.com>
2020-06-23 09:38:47 -0700

    update

--- js/navigation.js
+++ js/navigation.js
@@ -5,22 +5,47 @@
  * navigation support for dropdown menus.
  */
 
+
+/**
+ * Pass in an element and its CSS Custom Property that you want the value of.
+ * Optionally, you can determine what datatype you get back.
+ *
+ * @param {String} propKey
+ * @param {HTMLELement} element=document.documentElement
+ * @param {String} castAs='string'
+ * @returns {*}
+ */
+const getCSSCustomProp = (propKey, element = document.documentElement, castAs = 'string') => {
+    let response = getComputedStyle(element).getPropertyValue(propKey);
+  
+    // Tidy up the string if there's something to work with
+    if (response.length) {
+      response = response.replace(/\'|"/g, '').trim();
+    }
+  
+    // Convert the response into a whatever type we wanted
+    switch (castAs) {
+      case 'number':
+      case 'int':
+        return parseInt(response, 10);
+      case 'float':
+        return parseFloat(response, 10);
+      case 'boolean':
+      case 'bool':
+        return response === 'true' || response === '1';
+    }
+  
+    // Return the string response by default
+    return response;
+  };
+
+
+
 let toggle = document.querySelector('#toggle-nav'),
     menu = document.querySelector('#site-navigation'); // aka .nav
-    menuVisibility = getComputedStyle(menu).getPropertyValue('--menu-display')
-    console.log(menuVisibility);
     
 toggle.addEventListener('click', function(e) {
-    
+   console.log( getCSSCustomProp('--menu-display', menu));
     e.preventDefault();
-    let menuVisibility = getComputedStyle(menu).getPropertyValue('--menu-display');
-    console.log(menuVisibility);
 
-    if (getComputedStyle(menu).getPropertyValue('--menu-display') == 'none') {
-        console.log('equals none');
-        menu.style.setProperty('--menu-display', 'flex');
-    } else {
-        console.log('hiding menu function');
-        menu.style.setProperty('--menu-display', 'none');
-    }
 });
