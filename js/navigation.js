9e9c4649236a44791c0d2656454be6b25a65bdd5
Ian Hernandez <ian.hernandez@dreamhost.com>
2020-06-23 09:28:11 -0700

    update

--- js/navigation.js
+++ js/navigation.js
@@ -8,13 +8,14 @@
 let toggle = document.querySelector('#toggle-nav'),
     menu = document.querySelector('#site-navigation'); // aka .nav
     menuVisibility = getComputedStyle(menu).getPropertyValue('--menu-display')
-
+    console.log(menuVisibility);
+    
 toggle.addEventListener('click', function(e) {
     
     e.preventDefault();
     let menuVisibility = getComputedStyle(menu).getPropertyValue('--menu-display')
     console.log(menuVisibility);
-    
+
     if (menuVisibility === 'none') {
         console.log('show menu fucntion');
         menu.style.setProperty('--menu-display', 'flex');
