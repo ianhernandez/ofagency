3aaf1a9c9415676103c379327374f4aa4ab04d5e
Ian Hernandez <ian.hernandez@dreamhost.com>
2020-06-26 17:54:40 -0700

    update

--- footer.php
+++ footer.php
@@ -13,17 +13,7 @@
 
 	<footer id="colophon" class="site-footer container">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ofagency' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ofagency' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ofagency' ), 'ofagency', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			OF Agency LA
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
